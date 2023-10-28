8c2f04b8c43c84163c3985797cc1a6993f3cee39
mikegrekov <svg200914@gmail.com>
2023-10-28 14:59:39 +0100

    autotest

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+  const openbr = bracketsConfig.map(a => a[0]); 
+  const closebr = bracketsConfig.map(a => a[1]); 
+
+  const arr = str.split('');
+  const arrbra = [];
+  let popval = '';
+
+  for (let i = 0; i <= arr.length; i += 1) {
+    if (openbr.includes(arr[i]) && closebr.includes(arr[i]) && !arrbra.includes(arr[i])) {
+      arrbra.push(arr[i]); //console.log(arrbra, '<')  
+      continue};    
+    if (openbr.includes(arr[i]) && !closebr.includes(arr[i])) {
+      arrbra.push(arr[i]); //console.log(arrbra)  
+      continue};
+    if (closebr.includes(arr[i])) {
+        popval = arrbra.pop();   //console.log(popval)     
+        if (popval === undefined || popval !== openbr[closebr.indexOf(arr[i])]) return false;
+        continue;
+    };
+  }
+  return true;
 }
